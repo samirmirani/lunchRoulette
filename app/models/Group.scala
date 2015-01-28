5c4ada8dbcdc753ea782ea5a31609d053a0101dd
Samir Mirani <samir.mirani@gmail.com>
2015-01-28 10:48:37 -0800

    Added a bit more polish to the code.

--- app/models/Group.scala
+++ app/models/Group.scala
@@ -19,56 +19,52 @@ class Groups(users: List[User]) {
   private var _groups : List[Group] = Nil
 
 
-  def groupSize : Int = {
-    var random = scala.util.Random;
-    val range = 3 to 4
-    random.nextInt(range.length)
-    3
-  }
-
+  def groupSize : Int = 3
   /**
    * This will define the members of these groups.
    *
    * @param users
    * @return
    */
-  def fillGroups() : Unit = {
+  def fillGroups() : List[Group] = {
     //This entire section can be done better. More functional, have to revist this.
     var userCount : Int = _users.size
     System.out.println("userCount")
     System.out.println(userCount)
     do {
-      var sizeOfgroup = groupSize
-      System.out.println("sizeOfgroup")
-      System.out.println(sizeOfgroup)
-      var members = if (_users.size <=  sizeOfgroup + 2) {
-        //if there is a remainder in the group size just return the list. This way groups will maintain a minuim of at least 3
+
+      var members = if (_users.size <=  groupSize + (groupSize - 1)) {
+        //if there is a remainder in the group size just return the list. This way groups will maintain a min of at least the grpsize
         val members = _users.toList
-        _users = _users.drop(sizeOfgroup + 2)
-        userCount = userCount - sizeOfgroup - 2
+        _users = _users.drop(groupSize + groupSize - 1)
+        userCount = userCount - groupSize -  (groupSize + 1)
         members
       } else {
         //cut the list by the group size
-        val members = _users.slice(0, sizeOfgroup).toList
-        _users = _users.drop(sizeOfgroup)
-        userCount = userCount - sizeOfgroup
+        val members = _users.slice(0, groupSize).toList
+        _users = _users.drop(groupSize)
+        userCount = userCount - groupSize
         members
       }
 
+
+      System.out.println("Number of users in this group")
+      System.out.println(members.size)
 
       System.out.println("Members")
       System.out.println(members)
       _groups = _groups:+ new Group(members)
     } while (userCount > 0)
-  }
 
-  lazy val groups = {
     _groups
   }
 
-  fillGroups()
+  lazy val groups : List[Group] = {
+    fillGroups()
+  }
+
   //some debug stuff
-  var groupCount : Int = _groups.size
+  var groupCount : Int = groups.size
   System.out.println("groupCount")
   System.out.println(groupCount)
 
